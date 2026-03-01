6b189975908b53455a57041abbb6e5bee3b4f58b
Aether926 <warmsteve122@gmail.com>
2026-03-01 23:37:16 +0800

    check-email false

--- app/api/auth/check-email/route.ts
+++ app/api/auth/check-email/route.ts
@@ -1,3 +1,7 @@
+export async function POST() {
+    return Response.json({ exists: false });
+}
+
 // import { NextResponse } from "next/server";
 // import { createClient } from "@supabase/supabase-js";
 
