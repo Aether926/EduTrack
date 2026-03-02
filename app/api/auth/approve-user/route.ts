aae34153f1704bfce2968bd99a5f160ea8b46f5e
Aether926 <warmsteve122@gmail.com>
2026-03-02 09:29:33 +0800

    vercel fix (minor changes)

--- app/api/auth/approve-user/route.ts
+++ app/api/auth/approve-user/route.ts
@@ -1,10 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/app/generated/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest) {
     const { id } = await req.json() as { id: string };
-    await prisma.user.update({ where: { id }, data: { approved: true } });
+    await prisma.user.update({ where: { id }, data: { status: "approved" } });
     return NextResponse.json({ message: "Approved" }, { status: 200 });
 }
